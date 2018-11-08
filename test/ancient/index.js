e648bbd53606f4bc650790f35d18d0d3f5db54e4
EnixCoda <enixcoda@gmail.com>
2018-11-08 20:38:18 +0800

    test: add tests

---
+++ test/ancient/index.js
@@ -0,0 +1,5 @@
+const React = require("react")
+const ReactTestRenderer = require("react-test-renderer")
+const { test } = require("../boot/")
+
+test(React, ReactTestRenderer)
---
+++ test/latest/index.js
@@ -0,0 +1,5 @@
+const React = require("react")
+const ReactTestRenderer = require("react-test-renderer")
+const { test } = require("../boot/dist")
+
+test(React, ReactTestRenderer)
